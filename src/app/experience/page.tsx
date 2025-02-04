080c678bf23536685614bfa5072706a99bf347ae
sanju1098 <kumar.sanjay101998@gmail.com>
2025-02-04 21:20:10 +0530

    Updated Experience Section

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,33 +1,17 @@
-import { BriefcaseBusiness } from "lucide-react";
+import { workExperience } from "@src/config/experience";
+import { Award, Briefcase, BriefcaseBusiness, Calendar } from "lucide-react";
 
 export default function Skills() {
-  // const experiences = [
-  //   {
-  //     title: "Your Position",
-  //     company: "Company Name",
-  //     period: "2022 - Present",
-  //     description:
-  //       "Description of your responsibilities and achievements in this role.",
-  //   },
-  //   {
-  //     title: "Your Position 2",
-  //     company: "Company Name 2",
-  //     period: "2022 - Present",
-  //     description:
-  //       "Description of your responsibilities and achievements in this role.",
-  //   },
-  //   // Add more experiences as needed
-  // ];
   return (
     <>
       <div className="text-3xl font-bold text-primary flex justify-center items-center gap-2 text-center mt-4">
         Experience <BriefcaseBusiness className="w-9 h-9" />
       </div>
       <div className="space-y-8 animate-fade-in">
-        {/* {experiences.map((exp, index) => (
+        {workExperience.map((exp: any, index: number) => (
           <div
             key={index}
-            className="bg-card rounded-lg p-6 md:p-8 relative overflow-hidden group hover:border-primary border-2 transition-colors duration-300">
+            className="bg-card rounded-lg p-6 md:p-8 mx-12 my-6 relative overflow-hidden group hover:border-primary border-2 transition-colors duration-300">
             <div className="absolute top-0 left-0 w-1 h-full bg-gradient-to-b from-primary to-accent transform origin-top scale-y-0 group-hover:scale-y-100 transition-transform duration-300"></div>
             <div className="relative">
               <div className="flex items-center space-x-4 mb-4">
@@ -42,12 +26,14 @@ export default function Skills() {
               </div>
               <div className="flex items-center space-x-2 text-muted-foreground mb-4">
                 <Calendar className="w-4 h-4" />
-                <p>{exp.period}</p>
+                <p>
+                  {exp.startDate} - {exp.endDate}
+                </p>
               </div>
               <p className="text-foreground/80">{exp.description}</p>
             </div>
           </div>
-        ))} */}
+        ))}
       </div>
     </>
   );
---
+++ src/config/experience.tsx
@@ -0,0 +1,20 @@
+export const workExperience = [
+  {
+    company: "Unisys India Private Limited",
+    title: "Application Development Engineer",
+    startDate: "June 2023",
+    endDate: "Present",
+  },
+  {
+    company: "Harlalka Services India Pvt Ltd.",
+    title: "Front End Developer",
+    startDate: "October 2021",
+    endDate: "June 2023.",
+  },
+  {
+    company: "Sixth Circle Technologies.",
+    title: "Web Developer",
+    startDate: "February 2021",
+    endDate: "September 2021",
+  },
+];
